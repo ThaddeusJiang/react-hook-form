d722dabafe956f06f972cd19399a91dcab69d94e
TJ <thaddeusjiang@gmail.com>
2023-02-13 21:40:24 +0900

    modify storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,4 +4,18 @@ import '../tailwind.css';
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
+
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: 'rgb(243 244 246 / 100%)',
+      },
+      {
+        name: 'dark',
+        value: 'rgb(31 41 55 / 100%)',
+      },
+    ],
+  },
 };
